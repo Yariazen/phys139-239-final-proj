1ad8aad764416c4fe9fe8de64d612cd736b75034
Yariazen <35210313+Yariazen@users.noreply.github.com>
2023-02-27 14:03:43 -0800

    Switch to using pathlib

--- download-data.py
+++ download-data.py
@@ -1,18 +1,19 @@
 import io
 import os
 from os import path
+from pathlib import Path
 import zipfile
 
 if(not path.isfile('galaxy-zoo-the-galaxy-challenge.zip')):
     os.system('kaggle competitions download -c galaxy-zoo-the-galaxy-challenge')
 
 if(not path.exists(path.join('galaxy-zoo-the-galaxy-challenge', 'data'))):
-   os.mkdir('data')
+    Path(path.join('galaxy-zoo-the-galaxy-challenge', 'data')).mkdir(parents=True, exist_ok=True)
 
 with zipfile.ZipFile('galaxy-zoo-the-galaxy-challenge.zip', 'r') as zip_ref:
     for f in zip_ref.namelist():
         content = io.BytesIO(zip_ref.read(f))
         with zipfile.ZipFile(content, 'r') as zip_file:
-            zip_file.extractall('data')
+            zip_file.extractall(path.join('galaxy-zoo-the-galaxy-challenge', 'data'))
 
 
