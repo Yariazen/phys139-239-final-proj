50a4d024f0510088b24e83547da0e71c46b972d3
Yariazen <35210313+Yariazen@users.noreply.github.com>
2023-02-27 14:00:22 -0800

    Fix mkdir if not exist

--- download-data.py
+++ download-data.py
@@ -7,12 +7,12 @@ if(not path.isfile('galaxy-zoo-the-galaxy-challenge.zip')):
     os.system('kaggle competitions download -c galaxy-zoo-the-galaxy-challenge')
 
 if(not path.exists(path.join('galaxy-zoo-the-galaxy-challenge', 'data'))):
-   os.mkdir(path.join('galaxy-zoo-the-galaxy-challenge', 'data'))
+   os.mkdir('data')
 
 with zipfile.ZipFile('galaxy-zoo-the-galaxy-challenge.zip', 'r') as zip_ref:
     for f in zip_ref.namelist():
         content = io.BytesIO(zip_ref.read(f))
         with zipfile.ZipFile(content, 'r') as zip_file:
-            zip_file.extractall(path.join('galaxy-zoo-the-galaxy-challenge', 'data'))
+            zip_file.extractall('data')
 
 
